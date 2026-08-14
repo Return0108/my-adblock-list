b52e85e5dd3e16dc26eb952a98287af14d93d9ca
Return0108 <138360564+Return0108@users.noreply.github.com>
2026-08-15 00:25:28 +0800

    Update update.sh

--- filters/scripts/update.sh
+++ filters/scripts/update.sh
@@ -12,11 +12,10 @@ echo "开始合并规则"
 # 逐行读取链接下载内容追加到合并文件
 while IFS= read -r url; do
   if [[ -n "$url" ]]; then
-    echo "下载：$url"
+    echo "下载: $url"
     curl -sL --connect-timeout 20 "$url" >> "$OUTPUT_FILE"
     echo "" >> "$OUTPUT_FILE"
   fi
 done < "$SOURCE_FILE"
 
 echo "合并完成"
-
